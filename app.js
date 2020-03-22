aedb2a972c90c42d6c388f05b8209d70771fd81a
Your Name <roger_killer4@hotmail.comgit config --global user.email roger_killer4@hotmail.com>
2020-03-22 19:11:54 +0100

    we admit the render function to the deleteElement function

--- app.js
+++ app.js
@@ -4,44 +4,43 @@ import * as Tools from './tools.js';
 // JavaScript knows now your ul with the class .list
 const taskList = document.querySelector('.list');
 // we admit an eventlistener with a 'click' event and the deleteTask function
-taskList.addEventListener('click', Tools.deleteElement);
+taskList.addEventListener('click', deleteElement);
 
 // creat an elment with data from localStorage
-let ToDoList = JSON.parse(localStorage.getItem('ToDos'));
+let ToDoList = JSON.parse(localStorage.getItem('ToDos')); // ToDos is the key of localStorage
 if (ToDoList) {
-    for (let i = 0; i < ToDoList.length; i++) {
-    Tools.createElement(ToDoList[i].inhalt, taskList);
-    }
+    Tools.render(ToDoList, taskList);
 } else {
     ToDoList = [];
 };
-
 
 // JavaScript knows now your div with the class placeholder and the input tag
 const newEntry = document.querySelector('.placeholder input');
 // we admit an eventlistener with a 'keypress' event and the creatElement function
 newEntry.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
-            Tools.createElement(newEntry.value, taskList);
+            // Tools.render(newEntry.value, taskList);
             ToDoList.push({status:'open', inhalt: newEntry.value});
             newEntry.value = '';
-            creatArrayTask();
+            Tools.render(ToDoList, taskList);
         }
     }
 );
 
-// we creat the creatArrayTask function
-const creatArrayTask = () => {
-    const arrayTask = [];
-    let listLoop = document.querySelectorAll('.list__listitem');
-    for (let i = 0; i < listLoop.length; i++) {
-        let status = 'open';
-        let inhalt = listLoop[i].lastElementChild.previousElementSibling.innerHTML;
-        let taskObject = {status:status, inhalt:inhalt};
-        arrayTask.push(taskObject);
+
+// We creat the function deleteElement
+function deleteElement(event) {
+    if (event.target.matches('.list__btn__delete')) {
+        ToDoList.splice(event.target.parentNode.parentNode.id, 1);
+        Tools.render(ToDoList, taskList);
     }
-    const listJson = JSON.stringify(arrayTask);
-    localStorage.setItem('ToDos', listJson);
+   
 };
+// We create the filter function
+// function filterTasks() {
+//     ToDoList.filter(function(offen) {
+//         return offen.status == 'open';
+//     })
+// }
 
 
